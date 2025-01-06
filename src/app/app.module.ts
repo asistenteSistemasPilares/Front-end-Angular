cbc994251656527cbd548295ce6114740cbd2438
Programador Pilares <asistente.sistemas.pilares@gmail.com>
2025-01-06 17:42:30 -0500

    vr 0.0.1 - Carga de los plugins necearios para usar AdminLTE 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopageFoundComponent
   ],
   imports: [
     BrowserModule,
